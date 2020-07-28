7b22b999b8faa285faf902e09341d6b81b620dfa
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-07-28 23:14:48 +0100

    reuse detach EBS volume aws

--- scripts/module_04/create-ec2-instance.js
+++ scripts/module_04/create-ec2-instance.js
@@ -1,13 +1,13 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your regions */' })
+AWS.config.update({ region: 'eu-west-1' })
 
 // Declare local variables
 const ec2 = new AWS.EC2()
-const sgName = 'hamster_sg'
-const keyName = 'hamster_key'
-const instanceId = '/* TODO: Add the instance Id to stop */'
+const sgName = 'hamster_sg2'
+const keyName = 'hamster_key2'
+const instanceId = 'i-071100d95cedf661c'
 
 stopInstance(instanceId)
 .then(() => createInstance(sgName, keyName))
@@ -15,13 +15,13 @@ stopInstance(instanceId)
 
 function createInstance (sgName, keyName) {
   const params = {
-    ImageId: '/* TODO: Add ami id for aws linux */',
+    ImageId: 'ami-0c3e74fa87d2a4227',
     InstanceType: 't2.micro',
     KeyName: keyName,
     MaxCount: 1,
     MinCount: 1,
     Placement: {
-      AvailabilityZone: '/* TODO: Add the az from the instance that is stopping */'
+      AvailabilityZone: 'eu-west-1a'
     },
     SecurityGroups: [
       sgName
--- scripts/module_04/reuse-ebs-volume.js
+++ scripts/module_04/reuse-ebs-volume.js
@@ -1,28 +1,41 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'eu-west-1' })
 
 // Declare local variables
 const ec2 = new AWS.EC2()
-const volumeId = '/* TODO: Add the volume to detach/attach */'
-const instanceId = '/* TODO: Add the instance to attach to */'
+const volumeId = 'vol-0c28d47fdf0c69934'
+const instanceId = 'i-0abca935ae729b5e5'
 
 detachVolume(volumeId)
 .then(() => attachVolume(instanceId, volumeId))
+// attachVolume(instanceId, volumeId)
 
 function detachVolume (volumeId) {
-  // TODO: Configure detachVolume params
+  const params = {
+    VolumeId : volumeId
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Detach the volume
+    ec2.detachVolume(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
 
 function attachVolume (instanceId, volumeId) {
-  // TODO: Configure attachVolume params
+  const params = {
+    InstanceId : instanceId,
+    VolumeId : volumeId,
+    Device: '/dev/sdf'
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Attach the volume
+    ec2.attachVolume(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
