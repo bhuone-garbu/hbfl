e60fea87d2c9e1ccaf7a690e8cddd75aef6df41c
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-07-29 22:57:28 +0100

    cloud distribution and cloudfront updates

--- util/assets.js
+++ util/assets.js
@@ -1,4 +1,4 @@
-const base = 'https://hamster-bucket-buklau.s3-eu-west-1.amazonaws.com'
+const base = 'https://d2usc3hab5db3k.cloudfront.net'
 module.exports = {
   hamster1: base + '/images/hamster-1-final.png',
   hamster2: base + '/images/hamster-2-final.png',
