f51e27103196dd12f5f55578f25869f5bacefdb8
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-07-29 14:35:54 +0100

    configuring AWS route53 with the sdk

--- scripts/module_06/create-route53-recordset.js
+++ scripts/module_06/create-route53-recordset.js
@@ -1,21 +1,42 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'eu-west-1' })
 
 // Declare local variables
 const route53 = new AWS.Route53()
-const hzId = '/* TODO: Add your hostedzone id */'
+const hzId = '/hostedzone/Z01958832TWLHPB15LQXM'
 
 createRecordSet(hzId)
 .then(data => console.log(data))
 
 function createRecordSet (hzId) {
-  // TODO: Create params const
   // Link to ELB Regions:
   // https://docs.aws.amazon.com/general/latest/gr/elb.html
+  const params = {
+    HostedZoneId: hzId,
+    ChangeBatch: {
+      Changes: [
+        {
+          Action: 'CREATE',
+          ResourceRecordSet: {
+            Name: 'hbfl.online',
+            Type: 'A',
+            AliasTarget: {
+              DNSName: 'hamsterELB-224944669.eu-west-1.elb.amazonaws.com',
+              EvaluateTargetHealth: false,
+              HostedZoneId: 'Z32O12XQLNTSW2'
+            }
+          }
+        }
+      ]
+    }
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Create record set
+    route53.changeResourceRecordSets(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
