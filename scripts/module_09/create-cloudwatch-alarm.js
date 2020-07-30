2acd401eae703d8e22e8c151915ef6dd2258424c
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-07-30 13:24:39 +0100

    sqs, sns, cloudwatch etc. trialing out the sdk

--- scripts/module_09/create-cloudwatch-alarm.js
+++ scripts/module_09/create-cloudwatch-alarm.js
@@ -1,22 +1,48 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'eu-west-1' })
 
 // Declare local variables
 const cw = new AWS.CloudWatch()
 const alarmName = 'hamster-elb-alarm'
-const topicArn = '/* TODO: Add your SNS topic ARN */'
-const tg = '/* TODO: Add last part of Target Group ARN */'
-const lb = '/* TODO: Add last part of Load Balancer ARN */'
+const topicArn = 'arn:aws:sns:eu-west-1:988035987866:hamster-topic'
+const tg = 'targetgroup/hamsterTG/db354c8b2443726d'
+const lb = 'app/hamsterELB/3a47fe7b478bb234'
 
 createCloudWatchAlarm(alarmName, topicArn, tg, lb)
 .then(data => console.log(data))
 
 function createCloudWatchAlarm (alarmName, topicArn, tg, lb) {
-  // TODO: Create params const object
+  const params = {
+    AlarmName: alarmName,
+    ComparisonOperator: 'LessThanThreshold',
+    EvaluationPeriods: 1,
+    MetricName: 'HealthyHostCount',
+    Namespace: 'AWS/ApplicationELB',
+    Period: 60,
+    Threshold: 1,
+    AlarmActions: [
+      topicArn
+    ],
+    Dimensions: [
+      {
+        Name: 'TargetGroup',
+        Value: tg
+      },
+      {
+        Name: 'LoanBalancer',
+        Value: lb
+      }
+    ],
+    Statistic: 'Average',
+    TreatMissingData: 'breaching'
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Call putMetricAlarm
+    cw.putMetricAlarm(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
--- scripts/module_09/create-sns-subscription.js
+++ scripts/module_09/create-sns-subscription.js
@@ -1,21 +1,28 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'eu-west-1' })
 
 // Declare local variables
 const sns = new AWS.SNS()
 const type = 'sms'
-const endpoint = '/* TODO: Add your mobile number with country code */'
-const topicArn = '/* TODO: Add your sns topic arn */'
+const endpoint = '44123456789'
+const topicArn = 'arn:aws:sns:eu-west-1:988035987866:hamster-topic'
 
 createSubscription(type, topicArn, endpoint)
 .then(data => console.log(data))
 
 function createSubscription (type, topicArn, endpoint) {
-  // TODO: Create params const
+  const params = {
+    Protocol: type,
+    TopicArn: topicArn,
+    Endpoint: endpoint
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Subscribe
+    sns.subscribe(params, (err, data) => {
+      if (err) reject(err)
+      resolve(data)
+    })
   })
 }
